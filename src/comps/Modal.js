067b3b35db73ce0c548136cd0d7ab5b7cadbf21b
fahre2602 <33974338+fahre2602@users.noreply.github.com>
2020-08-01 23:40:14 +0700

    add caption on modal

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from "@material-ui/core/styles";
+import Favorite from "@material-ui/icons/Favorite";
+import CustomTabs from "./CustomTabs/CustomTabs";
 
-const Modal = ({ setSelectedImg, selectedImg }) => {
+import styles from "../assets/jss/material-kit-react/components/customTabsStyle";
+
+const useStyles = makeStyles(styles);
+
+const Modal = ({ setSelectedImg, selectedImg, setSelectedCaption, selectedCaption }) => {
 
   const handleClick = (e) => {
     if (e.target.classList.contains('backdrop')) {
       setSelectedImg(null);
+      setSelectedCaption(null);
     }
   }
+  
+  const classes = useStyles();
 
   return (
     <motion.div className="backdrop" onClick={handleClick}
@@ -18,6 +29,20 @@ const Modal = ({ setSelectedImg, selectedImg }) => {
         initial={{ y: "-100vh" }}
         animate={{ y: 0 }}
       />
+      <Container maxWidth="sm">
+        <CustomTabs
+          headerColor="primary"
+          tabs={[
+            {
+              tabName: "Caption",
+              tabIcon: Favorite,
+              tabContent: (
+              <h2 className={classes.captionContent}>{selectedCaption}</h2>
+              )
+            }
+          ]}
+        />
+      </Container>
     </motion.div>
   )
 }
